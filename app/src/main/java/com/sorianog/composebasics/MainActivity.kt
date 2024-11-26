53e7097d19df65823a9c1f2bc83a317e88d62372
sorianog <13261711+sorianog@users.noreply.github.com>
2024-11-25 18:23:01 -0800

    Completed Step 6 & 7: Created Row & Columns, Introduced State

--- app/src/main/java/com/sorianog/composebasics/MainActivity.kt
+++ app/src/main/java/com/sorianog/composebasics/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,22 +37,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Scaffold(modifier) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+//    Scaffold(modifier) { innerPadding ->
+//        Column(modifier = modifier.padding(vertical = 4.dp)) {
+//            for (name in names) {
+//                Greeting(
+//                    name = name,
+//                    modifier = Modifier.padding(innerPadding)
+//                )
+//            }
+//        }
+//    }
+
+    Surface(modifier) { ->
+        Column(modifier = modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(
+                    name = name
+                )
+            }
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = colorResource(id = android.R.color.holo_green_light)) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = colorResource(id = android.R.color.holo_green_light),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello,"
+                )
+                Text(
+                    text = "$name!"
+                )
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(
+                    text = if (expanded.value) "Show Less" else "Show more"
+                )
+            }
+        }
     }
 }
 
